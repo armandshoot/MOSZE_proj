325831d20690142d1fc9a74b1c5a8d26aba9e3e8
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2020-09-23 18:25:50 +0200

    Update with the parseUnit method

--- Fighter.h
+++ Fighter.h
@@ -1,6 +1,8 @@
 #pragma once
 #include<iostream>
 #include<string>
+#include<stdexcept>
+#include<fstream>
 
 class Fighter {
 
@@ -13,18 +15,22 @@ public:
 	Fighter(std::string iname, int ihp, int idmg) : name(iname), HP(ihp), DMG(idmg) {}
 	~Fighter() {}
 	void take_dmg(Fighter &enemy);
+	void deal_dmg (Fighter &enemy);
 
-	void deal_dmg (Fighter &enemy) ;
 	int getHP() const { return HP; }
 	int getDMG() const { return DMG; }
-
-
 	std::string getName() const { return name; }
 
 
-	friend std::ostream& operator<<(std::ostream& os, const Fighter& fi){
-		os << fi.getName() << ": HP: " << fi.getHP() << " DMG: " << fi.getDMG() << std::endl;
-		return os;
-	}
+	
+
+	friend std::ostream& operator<<(std::ostream& os, const Fighter& fi);
+
+	static Fighter parseUnit(std::string fname);
+		
+
+	
 	
 };
+
+
