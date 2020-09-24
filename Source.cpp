f84df88bcc0f26a52cb3dc7ad970b8d3229f8543
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2020-09-24 14:12:28 +0200

    MOST NAGYON JÓ

--- Source.cpp
+++ Source.cpp
@@ -1,35 +1,30 @@
 #include"Fighter.h"
-#include<exception>
+
 void duel(Fighter attacker, Fighter defender) {
 
 	bool can_attack = true;
-	/*próba exception*/
-	if (attacker.getHP() == -1)
-		return;
+
 	while (attacker.getHP() != 0 && defender.getHP() != 0)
 	{
-		std::cout << attacker << defender;
-
 		if (can_attack) {
-			std::cout << attacker.getName() << "--->" << defender.getName() << std::endl;
+			/*std::cout << attacker.getName() << "--->" << defender.getName() << std::endl;*/
 			attacker.deal_dmg(defender);
 			can_attack = false;
 		}
 		else
 		{
-			std::cout << defender.getName() << "--->" << attacker.getName() << std::endl;
+			/*std::cout << defender.getName() << "--->" << attacker.getName() << std::endl;*/
 			attacker.take_dmg(defender);
 			can_attack = true;
 		}
 	}
-	std::cout << attacker << defender;
 	if (attacker.getHP() > defender.getHP())
 	{
-		std::cout << defender.getName() << " died. " << attacker.getName() << " wins.\n";
+		std::cout << attacker.getName() << " Wins. " <<"Remaining HP: "<< attacker.getHP() << ".\n";
 	}
 	else
 	{
-		std::cout << attacker.getName() << " died. " << defender.getName() << " wins.\n";
+		std::cout << defender.getName() << " Wins. " << "Remaining HP: " << defender.getHP() << ".\n";
 	}
 }
 
@@ -44,21 +39,20 @@ str-be szeletelek a lineból stringeket find és posal.
 
 
 
-int main(/*int argc, char* argv[]*/)
+int main(int argc, char* argv[])
 {
-	/**/
 
-
-	
-	//Fighter f1 = Fighter(argv[1], std::stoi(argv[2]), std::stoi(argv[3]));
-	//Fighter f2 = Fighter(argv[4], std::stoi(argv[5]), std::stoi(argv[6]));
-	//duel(f1, f2);
-
-	Fighter u1(Fighter::parseUnit("Kakarot.json"));
-	Fighter u2(Fighter::parseUnit("Vegita.json"));
-	duel(u1, u2);
-
-	
+	try {
+		Fighter u1(Fighter::parseUnit(argv[1]));
+		Fighter u2(Fighter::parseUnit(argv[2]));
+		duel(u1, u2);
+		
+	}
+	catch (const std::exception &e)
+	{
+		std::cout << e.what() << '\n';
+		return 0;
+	}
 
 	return 0;
 }
