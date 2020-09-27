f67eb99b4142b5064ae4554809c7c7ba76799cda
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2020-09-27 20:11:49 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -18,16 +18,3 @@ do
 	done
 done
 
-
-file2="Maple wins. Remaining HP: 30.
-Kakarot wins. Remaining HP: 280.
-Sally wins. Remaining HP: 5.
-Kakarot wins. Remaining HP: 270.
-Kakarot wins. Remaining HP: 290.
-Kakarot wins. Remaining HP: 300."
-
-if cmp -s "HELLO" "HELLO"; then
-    printf "HELLO"
-else
-    printf "NEM"
-fi
