3bae84a65eac38a53cb0ad7e3d0567b109d0d8b1
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2020-09-27 21:07:03 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -10,7 +10,7 @@ touch output.txt
 ./a.out units/Kakarot.json units/Maple.json >> output.txt
 ./a.out units/Kakarot.json units/Sally.json >> output.txt
 
-FC output.txt units good_output.txt
+fc output.txt super_good_output.txt
 
 if [ $? -eq 0 ]
 then
