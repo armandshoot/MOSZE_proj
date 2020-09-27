260de0effd5d4d07c66923e03ac1613bbcafa41d
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2020-09-27 21:33:07 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -10,7 +10,7 @@ touch output.txt
 ./a.out units/Kakarot.json units/Maple.json >> output.txt
 ./a.out units/Kakarot.json units/Sally.json >> output.txt
 
-diff output.txt super_good_output.txt
+diff output.txt output.txt
 
 if [ $? -eq 0 ]
 then
