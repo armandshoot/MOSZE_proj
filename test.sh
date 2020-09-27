644a86fa8df90f3dbce37b10d0279f9c8ba07666
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2020-09-27 19:56:58 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -18,11 +18,16 @@ do
 	done
 done
 
-file1=output.txt
-file2=good_output.txt
 
-if cmp -s "$file1" "$file2"; then
-    printf 'The file "%s" is the same as "%s"\n' "$file1" "$file2"
+file2="Maple wins. Remaining HP: 30.
+Kakarot wins. Remaining HP: 280.
+Sally wins. Remaining HP: 5.
+Kakarot wins. Remaining HP: 270.
+Kakarot wins. Remaining HP: 290.
+Kakarot wins. Remaining HP: 300."
+
+if cmp -s "$FILE" "$file2"; then
+    printf 'The file "%s" is the same as "%s"\n' "$FILE" "$file2"
 else
-    printf 'The file "%s" is different from "%s"\n' "$file1" "$file2"
+    printf 'The file "%s" is different from "%s"\n' "$FILE" "$file2"
 fi
