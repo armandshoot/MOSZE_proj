45fb83247a637bd42643e917c5e14f4280bbb4ce
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2020-09-28 11:55:26 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -9,7 +9,7 @@ rm -f output.txt
 ./a.out units/Kakarot.json units/Maple.json >> output.txt
 ./a.out units/Kakarot.json units/Sally.json >> output.txt
 
-diff --strip-trailing-cr output.txt good_output.txt
+diff --strip-trailing-cr output.txt exp_output.txt
 
 if [ $? -eq 0 ]
 then
