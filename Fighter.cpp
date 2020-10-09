d65b190f3462895ff3a57a2569c836abc1d83cfa
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2020-10-09 15:46:21 +0200

    update the attackspeed function

--- Fighter.cpp
+++ Fighter.cpp
@@ -11,6 +11,7 @@ void Fighter::deal_dmg(Fighter &enemy) {
 	enemy.take_dmg(*this);
 }
 
+
 std::ostream& operator<<(std::ostream& os, const Fighter& fi)
 {
 	os << fi.getName() << ": HP: " << fi.getHP() << "," << " DMG: " << fi.getDMG() << std::endl;
@@ -23,16 +24,15 @@ Fighter Fighter::parseUnit(std::string fname)
 	std::ifstream file;
 
 
-	std::string name = "";
-	int hp = -1;
-	int dmg = -1;
-
-
 	file.open(fname);
 	if (!file.good()) throw std::runtime_error("File cannot be opened!");
 
 	else {
 		int i = 0;
+		std::string name;
+		int hp;
+		int dmg;
+		double as;
 
 		std::string line;
 		while (std::getline(file, line))
@@ -40,6 +40,7 @@ Fighter Fighter::parseUnit(std::string fname)
 
 			std::string str1 = "";
 			std::string str2 = "";
+			std::string str3 = "";
 			if (line[0] != '{' && line[0] != '}')
 			{
 				if (i == 0)
@@ -53,16 +54,20 @@ Fighter Fighter::parseUnit(std::string fname)
 				{
 					str1 = line.substr(line.find(':'));
 					str2 = str1.substr(str1.find(' ') + 1, str1.rfind(',') - 2);
+					
 					if (i == 1)
 						hp = std::stoi(str2);
 					if (i == 2)
 						dmg = std::stoi(str2);
+					if (i == 3)
+						as = std::stod(str2);
 				}
 				i++;
 			}
 		}
 		file.close();
-
+		std::cout << name <<" "<< hp<<" " << dmg<<" " << as << std::endl;
+		return Fighter(name, hp, dmg, as);
 	}
-	return Fighter(name, hp, dmg);
+	
 }
