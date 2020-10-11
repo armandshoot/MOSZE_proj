417226450456ea657dd98fa3cd37bf09c1a9af7f
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2020-10-11 17:22:33 +0200

    implement 10.11 update_WORKING

--- Fighter.h
+++ Fighter.h
@@ -6,22 +6,30 @@
 
 class Fighter {
 
-	int HP;
-	const int DMG;
+	double MaxHP;
+	double HP;
+	double DMG;
 	const std::string name;
+	int level = 1;
+
 
 public:
 
-	Fighter(std::string iname, int ihp, int idmg) : name(iname), HP(ihp), DMG(idmg) {}
+	int exp = 0;
+
+	Fighter(std::string iname, double ihp, int idmg) : name(iname), HP(ihp), DMG(idmg),MaxHP(ihp) {}
 	~Fighter() {}
 	void take_dmg(Fighter &enemy);
 	void deal_dmg(Fighter &enemy);
 
-	int getHP() const { return HP; }
-	int getDMG() const { return DMG; }
+	double getHP()  { return HP; }
+	double getDMG()  { return DMG; }
+	int getLVL() { return level; }
+	int getXP() { return exp; }
+
 	std::string getName() const { return name; }
 
-
+	void levelUP(Fighter *unit);
 
 
 	friend std::ostream& operator<<(std::ostream& os, const Fighter& fi);
