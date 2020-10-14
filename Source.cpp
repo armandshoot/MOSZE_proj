ad749b73f15eb3e3e4ef5fa365bb7f2e34127c16
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2020-10-14 16:52:06 +0200

    working 10.14

--- Source.cpp
+++ Source.cpp
@@ -1,4 +1,5 @@
 #include"Fighter.h"
+#include"Jsonparser.h"
 
 void duel(Fighter attacker, Fighter defender) {
 
@@ -7,13 +8,12 @@ void duel(Fighter attacker, Fighter defender) {
 	while (attacker.getHP() != 0 && defender.getHP() != 0)
 	{
 		if (can_attack) {
-			
+
 			attacker.deal_dmg(defender);
 			can_attack = false;
 		}
 		else
 		{
-			
 			attacker.take_dmg(defender);
 			can_attack = true;
 		}
@@ -33,9 +33,19 @@ int main(int argc, char* argv[])
 {
 
 	try {
-		Fighter u1(Fighter::parseUnit(argv[1]));
-		Fighter u2(Fighter::parseUnit(argv[2]));
+		std::map<std::string, std::string>map1 = Jsonparser::parseJson(argv[1]);
+
+		std::map<std::string, std::string>map2 = Jsonparser::parseJson(argv[2]);
+
+		Fighter u1(map1["name"], std::stoi (map1["hp"]),std::stof( map1["dmg"]));
+		Fighter u2(map2["name"], std::stoi (map2["hp"]),std::stof( map2["dmg"]));
+
+//		std::cout << u1.getName() << "\t" << u1.getHP() << "\t" << u1.getDMG() << "\t" << std::endl;
+//		std::cout << u2.getName() << "\t" << u2.getHP() << "\t" << u2.getDMG() << "\t" << std::endl;
+
 		duel(u1, u2);
+		//std::cout << u1.getName() <<"\t"<< u1.getHP() << "\t" << u1.getDMG() << "\t" << std::endl;
+		//std::cout << u2.getName()<<"\t" << u2.getHP() << "\t" << u2.getDMG() << "\t" << std::endl;
 
 	}
 	catch (const std::exception &e)
@@ -43,7 +53,19 @@ int main(int argc, char* argv[])
 		std::cout << e.what() << std::endl;
 		return 1;
 	}
+		
+		//std::cout << u1.getName() <<"\t"<< u1.getHP() << "\t" << u1.getDMG() << "\t" << std::endl;
+		//std::cout << u2.getName()<<"\t" << u2.getHP() << "\t" << u2.getDMG() << "\t" << std::endl;
 
+		//std::cout << "#\n";
+		//for (auto&j : mymap)
+		//	std::cout << j.first << "\t" << j.second << std::endl;
+		//std::cout << "#\n";
+		//Fighter f1(mymap["name"],std::stoi(mymap["hp"]),std::stof(mymap["dmg"]));
+
+		//Fighter m1(mymap["name"], std::stoi(mymap["hp"]), std::stof(mymap["dmg"]));
+
+		///*működik */
+		//std::cout << f1.getName() <<"\t"<< f1.getHP() << "\t" << f1.getDMG() << "\t" << std::endl;
 	return 0;
 }
-
