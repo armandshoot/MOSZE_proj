1f431f1c0fba053376d1ef6d600d37765bd84dd8
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2020-10-15 18:18:29 +0200

    Create valgrind.sh

---
+++ valgrind.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+IFS=$'\n'
+echo "$(valgrind --leak-check=yes --log-file=memory_leak.txt ./a.out player.json vader.json)"
+result="$(cat ./memory_leak.txt)"
+echo $result
+errors="$(echo $result | sed 's/^.*ERROR SUMMARY: \([0-9]*\) errors.*$/\1/')"
+if [ "$errors" == "0" ]
+then
+    echo
+    echo "No memory leaks found."
+else
+    echo "Memory leaks found."
+    exit 1
+fi
