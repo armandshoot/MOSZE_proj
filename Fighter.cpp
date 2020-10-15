db4e1878012d384e4e411457256a1f6b552e2946
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2020-10-15 17:12:25 +0200

    update simpler version

--- Fighter.cpp
+++ Fighter.cpp
@@ -11,6 +11,45 @@ void Fighter::deal_dmg(Fighter &enemy) {
 	enemy.take_dmg(*this);
 }
 
+void Fighter::duel(Fighter *enemy)
+{
+	bool can_attack = true;
+
+	double aCD = this->getCD();
+	double bCD = enemy->getCD();
+
+	double rest_a=0;
+	double rest_b=0;
+
+	this->deal_dmg(*enemy);
+	this->take_dmg(*enemy);
+
+	while (this->getHP() != 0 && enemy->getHP() != 0)
+	{
+		if (aCD + rest_a < bCD + rest_b) {
+			rest_a += aCD;
+			this->deal_dmg(*enemy);
+		}
+		else if (aCD + rest_a > bCD + rest_b) {
+			rest_b += bCD;
+			this->take_dmg(*enemy);
+		}
+		else { 
+			rest_a += aCD;
+			this->deal_dmg(*enemy);
+		}
+	}
+	if (this->getHP() > enemy->getHP())
+	{
+		std::cout << this->getName() << " wins. " << "Remaining HP: " << this->getHP() << std::endl;
+	}
+	else
+	{
+		std::cout << enemy->getName() << " wins. " << "Remaining HP: " << enemy->getHP() << std::endl;
+	}
+
+
+}
 
 std::ostream& operator<<(std::ostream& os, const Fighter& fi)
 {
@@ -32,7 +71,7 @@ Fighter Fighter::parseUnit(std::string fname)
 		std::string name;
 		int hp;
 		int dmg;
-		double as;
+		double attackcooldown;
 
 		std::string line;
 		while (std::getline(file, line))
@@ -40,7 +79,6 @@ Fighter Fighter::parseUnit(std::string fname)
 
 			std::string str1 = "";
 			std::string str2 = "";
-			std::string str3 = "";
 			if (line[0] != '{' && line[0] != '}')
 			{
 				if (i == 0)
@@ -54,20 +92,18 @@ Fighter Fighter::parseUnit(std::string fname)
 				{
 					str1 = line.substr(line.find(':'));
 					str2 = str1.substr(str1.find(' ') + 1, str1.rfind(',') - 2);
-					
 					if (i == 1)
 						hp = std::stoi(str2);
 					if (i == 2)
 						dmg = std::stoi(str2);
 					if (i == 3)
-						as = std::stod(str2);
+						attackcooldown = std::stod(str2);
 				}
 				i++;
 			}
 		}
 		file.close();
-		std::cout << name <<" "<< hp<<" " << dmg<<" " << as << std::endl;
-		return Fighter(name, hp, dmg, as);
+		return Fighter(name, hp, dmg,attackcooldown);
 	}
-	
+
 }
