e44997b9398167480218506e514364dffc461143
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2020-10-15 18:21:09 +0200

    Update valgrind.sh

--- valgrind.sh
+++ valgrind.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 IFS=$'\n'
-echo "$(valgrind --leak-check=yes --log-file=memory_leak.txt ./a.out player.json vader.json)"
+echo "$(valgrind --leak-check=yes --log-file=memory_leak.txt ./a.out Maple.json Sally.json)"
 result="$(cat ./memory_leak.txt)"
 echo $result
 errors="$(echo $result | sed 's/^.*ERROR SUMMARY: \([0-9]*\) errors.*$/\1/')"
