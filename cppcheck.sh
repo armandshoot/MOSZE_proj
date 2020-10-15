f60dcade07c1fb3388d3aecc767b43006198a187
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2020-10-15 21:22:02 +0200

    Update cppcheck.sh

--- cppcheck.sh
+++ cppcheck.sh
@@ -6,7 +6,7 @@ IFS=$'\n'
 echo "$(cppcheck *.cpp --enable=warning --output-file=cppcheck_errors.txt)"
 	if  [ ! -s .cppcheck_errors.txt ]
 	then
-			echo "Not found any errors and warnings."
+			echo "No performance or style error."
 			 echo "$(cppcheck *.cpp --enable=performance,style --output-file=cppcheck_performance.txt)"
 		
 		if [ ! -s cppcheck_performance.txt ]
