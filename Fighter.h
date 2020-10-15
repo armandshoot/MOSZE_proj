5cf6ba4c9aadb6a0ad699df33553ccae1040cac7
Varga Máté <matevarga96@gmail.com>
2020-10-15 21:47:25 +0200

    documentation

--- Fighter.h
+++ Fighter.h
@@ -6,15 +6,37 @@
 
 class Fighter {
 
-	int HP;
-	const int DMG;
-	const double attackcooldown;
-	const std::string name;
+	int HP
+	/**
+	 * Characters health points.
+	 */;
+	const int DMG
+	/**
+	 * Characters damage.
+	 */;
+	const double attackcooldown
+	/**
+	 * Characters attack speed.
+	 */;
+	const std::string name
+	/**
+	 * Characters name.
+	 */;
 
-	void take_dmg(Fighter &enemy);
-	void deal_dmg(Fighter &enemy);
+	void take_dmg(Fighter &enemy)
+	/**
+	 * Method for character taking damage.
+	 */;
+	void deal_dmg(Fighter &enemy)
+	/**
+	 * Method for character dealing damage.
+	 */;
 	
 public:
+/**
+ * A class that contains the attributes of fighters.
+ * They have HP, Damage, attackcooldown which they get from input, and their own name.
+ */
 
 	Fighter(std::string iname, int ihp, int idmg, double acd) : name(iname), HP(ihp), DMG(idmg), attackcooldown(acd) {}
 	~Fighter() {}
@@ -25,13 +47,22 @@ public:
 	double getCD() const { return attackcooldown; }
 	std::string getName() const { return name; }
 
-	void duel(Fighter *enemy);
+	void duel(Fighter *enemy)
+	/**
+	 * Method for 2 characters fighting.
+	 */;
 
 
 
-	friend std::ostream& operator<<(std::ostream& os, const Fighter& fi);
+	friend std::ostream& operator<<(std::ostream& os, const Fighter& fi)
+	/**
+	 * Operator overload to make the class printable.
+	 */;
 
-	static Fighter parseUnit(std::string fname);
+	static Fighter parseUnit(std::string fname)
+	/**
+	 * Static method to process input json files.
+	 */;
 
 
 
