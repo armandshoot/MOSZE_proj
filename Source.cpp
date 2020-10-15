babe91b3cbec0ada69592da8e7f062184a993ba9
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2020-10-15 18:38:54 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -40,12 +40,10 @@ int main(int argc, char* argv[])
 		Fighter u1(map1["name"], std::stoi (map1["hp"]),std::stof( map1["dmg"]));
 		Fighter u2(map2["name"], std::stoi (map2["hp"]),std::stof( map2["dmg"]));
 
-//		std::cout << u1.getName() << "\t" << u1.getHP() << "\t" << u1.getDMG() << "\t" << std::endl;
-//		std::cout << u2.getName() << "\t" << u2.getHP() << "\t" << u2.getDMG() << "\t" << std::endl;
+
 
 		duel(u1, u2);
-		//std::cout << u1.getName() <<"\t"<< u1.getHP() << "\t" << u1.getDMG() << "\t" << std::endl;
-		//std::cout << u2.getName()<<"\t" << u2.getHP() << "\t" << u2.getDMG() << "\t" << std::endl;
+
 
 	}
 	catch (const std::exception &e)
@@ -54,18 +52,6 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 		
-		//std::cout << u1.getName() <<"\t"<< u1.getHP() << "\t" << u1.getDMG() << "\t" << std::endl;
-		//std::cout << u2.getName()<<"\t" << u2.getHP() << "\t" << u2.getDMG() << "\t" << std::endl;
 
-		//std::cout << "#\n";
-		//for (auto&j : mymap)
-		//	std::cout << j.first << "\t" << j.second << std::endl;
-		//std::cout << "#\n";
-		//Fighter f1(mymap["name"],std::stoi(mymap["hp"]),std::stof(mymap["dmg"]));
-
-		//Fighter m1(mymap["name"], std::stoi(mymap["hp"]), std::stof(mymap["dmg"]));
-
-		///*működik */
-		//std::cout << f1.getName() <<"\t"<< f1.getHP() << "\t" << f1.getDMG() << "\t" << std::endl;
 	return 0;
 }
