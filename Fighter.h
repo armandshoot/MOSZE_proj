76158d23662f3620e7e260f9c0eda8a91c210c2b
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2020-10-24 21:10:05 +0200

    Update Fighter.h

--- Fighter.h
+++ Fighter.h
@@ -3,26 +3,19 @@
 #include<string>
 #include<stdexcept>
 #include<fstream>
+#include<cmath>
 
-<<<<<<< HEAD
-	double MaxHP;
-	double HP;
-	double DMG;
-	const std::string name;
-	int level = 1;
-	int exp = 0;
-
-=======
 class Fighter {
-	int HP
+
+	double HP
 		/**
 		 * Characters health points.
 		 */;
-	const int DMG
+	double DMG
 		/**
 		 * Characters damage.
 		 */;
-	const double attackcooldown
+	double attackcooldown
 		/**
 		 * Characters attack speed.
 		 */;
@@ -38,41 +31,37 @@ class Fighter {
 		/**
 		 * Method for character dealing damage.
 		 */;
->>>>>>> Master_copy
 
-	public:
-	/**
-	 * A class that contains the attributes of fighters.
-	 * They have HP, Damage, attackcooldown which they get from input, and their own name.
-	 */
-	Fighter(std::string iname, int ihp, int idmg, double acd) : name(iname), HP(ihp), DMG(idmg), attackcooldown(acd) {}
+
+	double MaxHP;
+
+	int level = 1;
+	int exp = 0;
+
+
+	void levelUP();
+
+	
+
+public:
+	
+	Fighter(const std::string &iname, double ihp, int idmg, double acd) : name(iname), HP(ihp), DMG(idmg),MaxHP(ihp), attackcooldown(acd) {}
 	~Fighter() {}
 
-<<<<<<< HEAD
-	void take_dmg(Fighter &enemy);
-	void deal_dmg(Fighter &enemy);
 
 	double getHP() const { return HP; }
 	double getDMG() const { return DMG; }
+	double getCD() const { return attackcooldown; }
 	int getLVL() const { return level; }
 	int getXP() const { return exp; }
 
-	std::string getName() const { return name; }
-
-	friend std::ostream& operator<<(std::ostream& os, const Fighter& fi);
-	static Fighter parseUnit(std::string fname);
-=======
-
-	int getHP() const { return HP; }
-	int getDMG() const { return DMG; }
-	double getCD() const { return attackcooldown; }
 	std::string getName() const { return name; }
 
 	Fighter& duel(Fighter *enemy)/**
 		 * Method for 2 characters fighting.
 		 */;
 
-		
+
 
 	friend std::ostream& operator<<(std::ostream& os, const Fighter& fi)
 		/**
@@ -83,9 +72,8 @@ class Fighter {
 		/**
 		 * Static method to process input json files.
 		 */;
->>>>>>> Master_copy
-
 
 
 
 };
+
