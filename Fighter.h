35303cd30659abc8dbbd6f96066b2d69997b8efb
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2020-10-24 20:05:41 +0200

    Update Fighter.h

--- Fighter.h
+++ Fighter.h
@@ -3,49 +3,64 @@
 #include<string>
 #include<stdexcept>
 #include<fstream>
-#include<cmath>
+
 class Fighter {
-
-	double MaxHP;
-	double HP;
-	double DMG;
-	const std::string name;
-	int level = 1;
-	int exp = 0;
+	int HP
+		/**
+		 * Characters health points.
+		 */;
+	const int DMG
+		/**
+		 * Characters damage.
+		 */;
 	const double attackcooldown
-	/**
-	 * Characters attack speed.
-	 */;
-
+		/**
+		 * Characters attack speed.
+		 */;
+	const std::string name
+		/**
+		 * Characters name.
+		 */;
 	void take_dmg(Fighter &enemy)
-	/**
-	 * Method for character taking damage.
-	 */;
+		/**
+		 * Method for character taking damage.
+		 */;
 	void deal_dmg(Fighter &enemy)
+		/**
+		 * Method for character dealing damage.
+		 */;
+
+	public:
 	/**
-	 * Method for character dealing damage.
-	 */;
-
-	void levelUP();
-
-public:
-	
-	Fighter(const std::string &iname, double ihp, int idmg) : name(iname), HP(ihp), DMG(idmg),MaxHP(ihp) {}
+	 * A class that contains the attributes of fighters.
+	 * They have HP, Damage, attackcooldown which they get from input, and their own name.
+	 */
+	Fighter(std::string iname, int ihp, int idmg, double acd) : name(iname), HP(ihp), DMG(idmg), attackcooldown(acd) {}
 	~Fighter() {}
 
-	double getHP() const { return HP; }
-	double getDMG() const { return DMG; }
-	int getLVL() const { return level; }
-	int getXP() const { return exp; }
 
-
-	
-
+	int getHP() const { return HP; }
+	int getDMG() const { return DMG; }
 	double getCD() const { return attackcooldown; }
+	std::string getName() const { return name; }
 
+	Fighter& duel(Fighter *enemy)/**
+		 * Method for 2 characters fighting.
+		 */;
+
+		
+
+	friend std::ostream& operator<<(std::ostream& os, const Fighter& fi)
+		/**
+		 * Operator overload to make the class printable.
+		 */;
+
+	static Fighter parseUnit(std::string fname)
+		/**
+		 * Static method to process input json files.
+		 */;
 
 
 
 
 };
-
