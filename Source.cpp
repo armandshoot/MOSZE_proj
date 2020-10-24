cc77cf4219771f570ae0f8ca128a41ea10dd6076
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2020-10-24 20:05:58 +0200

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,40 +1,5 @@
 #include"Fighter.h"
 
-<<<<<<< HEAD
-void duel(Fighter attacker, Fighter defender) {
-
-	bool can_attack = true;
-
-
-	while (attacker.getHP() != 0 && defender.getHP() != 0)
-	{
-		if (can_attack) {
-
-			attacker.deal_dmg(defender); 
-			can_attack = false;
-
-			
-		}
-		else
-		{
-
-			attacker.take_dmg(defender);
-
-			can_attack = true;
-		}
-	}
-	if (attacker.getHP() > defender.getHP())
-	{
-		std::cout << attacker.getName() << " wins. " << "Remaining HP: " << attacker.getHP() << std::endl;
-	}
-	else
-	{
-		std::cout << defender.getName() << " wins. " << "Remaining HP: " << defender.getHP() << std::endl;
-	}
-
-}
-=======
->>>>>>> master
 
 
 int main(int argc, char* argv[])
@@ -43,21 +8,16 @@ int main(int argc, char* argv[])
 	try {
 		Fighter u1(Fighter::parseUnit(argv[1]));
 		Fighter u2(Fighter::parseUnit(argv[2]));
-		u1.duel(&u2);
-		/**
-	 	* Exception handling. An exception thrown when a file related error occurs.
-	 	*/
+
+		Fighter *outcome=&u1.duel(&u2);
+		std::cout << outcome->getName() << " wins. " << "Remaining HP: " << outcome->getHP() << std::endl;
 
 	}
 	catch (const std::exception &e)
 	{
 		std::cout << e.what() << std::endl;
 		return 1;
-		/**
-	 	* Exception handling. Catches and warns about an error occurring.
-	 	*/
 	}
-
 
 	return 0;
 }
