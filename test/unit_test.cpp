a73ba531d3e54d1b2bb3150150f1a1992bbf2f0b
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2020-10-25 17:28:35 +0100

    add gtest

---
+++ test/unit_test.cpp
@@ -0,0 +1,43 @@
+#include <gtest/gtest.h>
+#include <string>
+#include <map>
+#include <fstream>
+#include "../parser.h"
+
+TEST(ParserTest, TestingWithFileInput)
+{
+	std::string inputFilename = "units/Maple.json";
+	std::map<std::string, std::string> expected;
+	expected.insert(std::pair<std::string, std::string>("name", "Maple"));
+	expected.insert(std::pair<std::string, std::string>("hp", "150"));
+	expected.insert(std::pair<std::string, std::string>("dmg", "10"));
+
+
+    std::ifstream inputFile(inputFilename);
+
+	std::map<std::string, std::string> testingFunction = Jsonparser::parseJson(inputFile);
+
+    inputFile.close();
+
+	ASSERT_EQ(expected, testingFunction);
+}
+
+TEST(ParserTest, TestingWithFilenameInput)
+{
+	std::string inputFilename = "test/bad_unit.json";
+
+	ASSERT_THROW(Parser::ParseJsonFilename(inputFilename), std::runtime_error);
+}
+
+TEST(ParserTest, TestingWithStringInput)
+{
+	std::string inputString = "{\n\t\"name\"  :  \"Maple\",\n\t\"hp\":150,\n\t\"dmg\"   :    10,}";
+
+	ASSERT_THROW(Parser::ParseJsonString(inputString), std::runtime_error);
+}
+
+int main(int argc, char** argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
