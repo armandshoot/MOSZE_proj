3cee96f1c03c392d7ce74c02f62fa41e04f7f05c
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2020-10-25 21:45:37 +0100

    Update unit_test.cpp

--- test/unit_test.cpp
+++ test/unit_test.cpp
@@ -5,36 +5,35 @@
 #include <utility>
 #include "../Jsonparser.h"
 
-TEST(ParserTest, TestingWithFileInput)
+TEST(JsonparserTest, Test_parseJson_ifstream)
 {
 	std::string inputFilename = "units/Maple.json";
-	std::map<std::string, std::string> expected;
-	expected.insert(std::pair<std::string, std::string>("name", "Maple"));
-	expected.insert(std::pair<std::string, std::string>("hp", "150"));
-	expected.insert(std::pair<std::string, std::string>("dmg", "10"));
-
+	std::map<std::string, std::string> exp_output;
+	exp_output.insert(std::pair<std::string, std::string>("name", "Maple"));
+	exp_output.insert(std::pair<std::string, std::string>("hp", "150"));
+	exp_output.insert(std::pair<std::string, std::string>("dmg", "10"));
 
     std::ifstream inputFile(inputFilename);
 
-	std::map<std::string, std::string> testingFunction = Jsonparser::parseJson(inputFile);
+	std::map<std::string, std::string> test_output = Jsonparser::parseJson(inputFile);
 
     inputFile.close();
 
-	ASSERT_EQ(expected, testingFunction);
+	ASSERT_EQ(exp_output, test_output);
 }
 
-TEST(ParserTest, TestingWithFilenameInput)
+TEST(JsonparserTest, Test_parseJson_fname)
 {
 	std::string inputFilename = "test/bad_unit.json";
 	//std::string inputFilename = "units/Maple.json";
 	ASSERT_THROW(Jsonparser::parseJson(inputFilename), std::runtime_error);
 }
 
-TEST(ParserTest, TestingWithStringInput)
+TEST(JsonparserTest, Test_getmap)
 {
-	std::string inputString = "{\n\t\"name\"  :  \"Maple\",\n\t\"hp\":150,\n\t\"dmg\"   :    10,}";
+	std::string str = "{This is a bad Json}";
 
-	ASSERT_THROW(Jsonparser::parseJson(inputString), std::runtime_error);
+	ASSERT_THROW(Jsonparser::getmap(str), std::runtime_error);
 }
 
 int main(int argc, char** argv)
