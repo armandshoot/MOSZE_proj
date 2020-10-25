c6128a2475457e161251ed550974eb6a7b46d87f
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2020-10-25 17:34:12 +0100

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,25 +1,20 @@
 #include"Fighter.h"
+#include"Jsonparser.h"
 
-<<<<<<< HEAD
 void duel(Fighter attacker, Fighter defender) {
 
 	bool can_attack = true;
-
 
 	while (attacker.getHP() != 0 && defender.getHP() != 0)
 	{
 		if (can_attack) {
 
-			attacker.deal_dmg(defender); 
+			attacker.deal_dmg(defender);
 			can_attack = false;
-
-			
 		}
 		else
 		{
-
 			attacker.take_dmg(defender);
-
 			can_attack = true;
 		}
 	}
@@ -31,33 +26,30 @@ void duel(Fighter attacker, Fighter defender) {
 	{
 		std::cout << defender.getName() << " wins. " << "Remaining HP: " << defender.getHP() << std::endl;
 	}
-
 }
-=======
->>>>>>> master
 
 
 int main(int argc, char* argv[])
 {
 
 	try {
-		Fighter u1(Fighter::parseUnit(argv[1]));
-		Fighter u2(Fighter::parseUnit(argv[2]));
-		u1.duel(&u2);
-		/**
-	 	* Exception handling. An exception thrown when a file related error occurs.
-	 	*/
 
+		//Jsonparser parser_unit;
+		std::map<std::string, std::string>map1 = Jsonparser::parseJson(argv[1]);
+		std::map<std::string, std::string>map2 = Jsonparser::parseJson(argv[2]);
+
+
+
+		Fighter u1(map1["name"], std::stoi (map1["hp"]),std::stof( map1["dmg"]));
+		Fighter u2(map2["name"], std::stoi (map2["hp"]),std::stof( map2["dmg"]));
+		duel(u1, u2);
 	}
 	catch (const std::exception &e)
 	{
 		std::cout << e.what() << std::endl;
 		return 1;
-		/**
-	 	* Exception handling. Catches and warns about an error occurring.
-	 	*/
 	}
-
+		
 
 	return 0;
 }
