14e615d55dc930f86fa6097c2201fa8fa7058960
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2020-10-25 21:39:53 +0100

    Update Jsonparser.h

--- Jsonparser.h
+++ Jsonparser.h
@@ -3,15 +3,13 @@
 
 class Jsonparser
 {
-	/*mindent ami tudok elrejteni, mainben hívok egy osztály és onnan olvasok be egy vektorba*/
+	
 	enum type { Integer, Double, String };
-
-	static std::map<std::string, std::string> getmap(std::string str);
 	static type determine_type(std::string str);
 
 public:
 
-	
+	static std::map<std::string, std::string> getmap(std::string str);
 	static std::map<std::string, std::string> parseJson(std::string fname);
 	static std::map<std::string, std::string> parseJson(std::istream &input);
 
