ea3d7fd7aa761c2f90dcd8163e3a81f32a6e05d9
Richard Vas <58149185+RichardVas@users.noreply.github.com>
2020-10-25 22:08:21 +0100

    Update unit_test.cpp

--- test/unit_test.cpp
+++ test/unit_test.cpp
@@ -1,18 +1,17 @@
-#include "../Jsonparser.h"
 #include <gtest/gtest.h>
-
+#include <string>
+#include <map>
 #include <fstream>
 #include <utility>
+#include "../Jsonparser.h"
 
-
-
-TEST(JsonparserTest, Test_parseJson_ifstream_good)
+TEST(ParserTest, TestingWithFileInput)
 {
 	std::string inputFilename = "units/Maple.json";
 	std::map<std::string, std::string> expected;
 	expected.insert(std::pair<std::string, std::string>("name", "Maple"));
-	expected.insert(std::pair<std::string, std::string>("hp", "150"));
-	expected.insert(std::pair<std::string, std::string>("dmg", "10"));
+	expected.insert(std::pair<std::string, std::string>("hp", " 150,\r"));
+	expected.insert(std::pair<std::string, std::string>("dmg", " 10,\r"));
 
 
     std::ifstream inputFile(inputFilename);
@@ -24,30 +23,18 @@ TEST(JsonparserTest, Test_parseJson_ifstream_good)
 	ASSERT_EQ(expected, testingFunction);
 }
 
-TEST(JsonparserTest, Test_parseJson_fname_bad)
+TEST(ParserTest, TestingWithFilenameInput)
 {
 	std::string inputFilename = "test/bad_unit.json";
 	//std::string inputFilename = "units/Maple.json";
 	ASSERT_THROW(Jsonparser::parseJson(inputFilename), std::runtime_error);
 }
-TEST(JsonparserTest, Test_parseJson_fname_good)
-{
-	std::string inputFilename = "units/Maple.json";
-	std::map<std::string, std::string> exp_output;
-	exp_output.insert(std::pair<std::string, std::string>("name", "Maple"));
-	exp_output.insert(std::pair<std::string, std::string>("hp", "150"));
-	exp_output.insert(std::pair<std::string, std::string>("dmg", "10"));
-	
-	std::map<std::string, std::string> test_output = Jsonparser::parseJson(inputFile);
-	
-	ASSERT_EQ(exp_output, test_output);
-}
 
-TEST(JsonparserTest, Test_getmap_fail)
+TEST(ParserTest, TestingWithStringInput)
 {
-	std::string str = "{\n\t\"INVALID\" \"JSON\",\n\t,\n\t\"FORMAT\"   ,\n \"FAIL\": -1}";
+	std::string inputString = "{\n\t\"name\"  :  \"Maple\",\n\t\"hp\":150,\n\t\"dmg\"   :    10,}";
 
-	ASSERT_THROW(Jsonparser::getmap(str), std::runtime_error);
+	ASSERT_THROW(Jsonparser::parseJson(inputString), std::runtime_error);
 }
 
 int main(int argc, char** argv)
